b7b782339ab2c68aaffccbfbf6c2ed8e5f635ba2
Tobias Braack <tobias.braack@haw-hamburg.de>
2015-04-20 15:44:57 +0200

    First Implementation Testing serial communication

--- ampel/src/CommunnicationThread.cpp
+++ ampel/src/CommunnicationThread.cpp
@@ -7,14 +7,24 @@
 
 #include "CommunnicationThread.h"
 
+#define SENDER
 
 
-Communnication_Thread::Communnication_Thread() {
-
-
+Communnication_Thread::Communnication_Thread(Serial& ser) {
+	cout << "ctor Communication Test" << endl;
 }
 
 Communnication_Thread::~Communnication_Thread() {
 	// TODO Auto-generated destructor stub
+	cout << "dtor Communication Test" << endl;
 }
 
+void Communnication_Thread::execute(void*){
+
+
+}
+
+
+void Communnication_Thread::shutdown(){
+    cout << "Hal Test shutdown" << endl;
+}
--- ampel/src/CommunnicationThread.h
+++ ampel/src/CommunnicationThread.h
@@ -5,14 +5,26 @@
  *      Author: lukasTest
  */
 
+#include <stdint.h>
+#include <pthread.h>
+#include "lib/HAWThread.h"
+#include "Serial.h"
+
 #ifndef COMMUNNICATIONTHREAD_H_
 #define COMMUNNICATIONTHREAD_H_
 
-class Communnication_Thread {
+class Communnication_Thread: public HAWThread {
 public:
-	Communnication_Thread();
+	Communnication_Thread(Serial& ser);
 	virtual ~Communnication_Thread();
+
 private:
+	Communnication_Thread(const Communnication_Thread& b);      ///< Copy-Konstruktor. Privat, deshalb kann dieses Objekt nicht als "Call-by-value" uebergeben werden.
+	Communnication_Thread& operator=(Communnication_Thread& b);
+	virtual void execute(void*); ///< Geerbt aus HAWThread. Muss implementiert werden.
+	virtual void shutdown();
+
+	Serial& ser;
 
 };
 
