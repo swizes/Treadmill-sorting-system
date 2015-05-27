c1660c079f247ff7cc8da9668befef4863790e1c
Lukas Wendt <lukas.wendt@haw-hamburg.de>
2015-05-27 19:22:33 +0200

    Initialize Puk Class with Getter/Setters

---
+++ ampel/src/Puk.cpp
@@ -0,0 +1,21 @@
+/*
+ * Puk.cpp
+ *
+ *  Created on: 27.05.2015
+ *      Author: abn908
+ */
+
+#include "Puk.h"
+
+Puk::Puk() {
+	metal = false;
+	holeOnTop = false;
+	size = 0;
+
+}
+
+Puk::~Puk() {
+
+	// TODO Auto-generated destructor stub
+}
+
