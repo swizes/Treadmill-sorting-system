0a233e7e1efe78256b9025fd6e7efadea86f14fd
Ibo Sy <isy@bitsilk.com>
2015-06-09 18:47:42 +0200

    update hole detection

--- ampel/src/HoleDetector.cpp
+++ ampel/src/HoleDetector.cpp
@@ -7,8 +7,10 @@ using namespace std;
 	static int max_ground_height = 900;
 	static int min_puck_height = 2600;
 	static int min_hole_diff = 500;
-	static long max_runthrough_time_fast = 20000;
-	static long max_runthrough_time_slow = 50000;
+	static long max_runthrough_time_fast = 2;
+	static long max_runthrough_time_fast_ms = 2;
+	static long max_runthrough_time_slow = 5;
+	static long max_runthrough_time_slow_ms = 5;
 
 bool HoleDetector::detectHole(HAL *hal, bool runningSlow) {
 	int measured_max_height = 0;
@@ -16,20 +18,25 @@ bool HoleDetector::detectHole(HAL *hal, bool runningSlow) {
 	int mheight, puckheight0, puckheight1;
 	int elapsed_time = 0;
 	int max_time = runningSlow ? max_runthrough_time_slow : max_runthrough_time_fast;
+	int max_time_ms = runningSlow ? max_runthrough_time_slow_ms : max_runthrough_time_fast_ms;
 
-	/*
-	max_ground_height = 900;
-	min_puck_height = 2000;
-	min_hole_diff = 500;
-	max_runthrough_time_fast = 20000;
-	max_runthrough_time_slow = 50000;
-	 */
+	max_time *= 1000;
+	max_time += max_time_ms;
+
 	bool foundHole = false, puckComplete = false;
+
+	Timer timer;
+	timer.createTimer();
+	timer.setTimer(max_time, max_time_ms*1000);
+	struct timespec result;
 
 	// wait for puck entry
 	mheight = hal->get_height_measure();
 
 	while(mheight > min_puck_height) {
+		timer.getTime(&result);
+		elapsed_time = result.tv_sec * 1000;
+		elapsed_time += result.tv_nsec / 1000;
 		cout << "w1 (" << mheight << " > " << min_puck_height << ") [t: " << elapsed_time << "]" << endl;
 		mheight = hal->get_height_measure();
 	}
@@ -39,7 +46,10 @@ bool HoleDetector::detectHole(HAL *hal, bool runningSlow) {
 	// mark time puck entry
 
 	// wait for hole or time up
-	while ((mheight = hal->get_height_measure()) < (puckheight0 + min_hole_diff) && elapsed_time < max_time) {
+	while ((mheight = hal->get_height_measure()) < (puckheight0 + min_hole_diff) && elapsed_time > 0) {
+		timer.getTime(&result);
+		elapsed_time = result.tv_sec * 1000;
+		elapsed_time += result.tv_nsec / 1000;
 		cout << "w2 (" << mheight << " < " << (puckheight0 + min_hole_diff) << " [" << puckheight0 << "+" <<  min_hole_diff << "] [t: " << elapsed_time << "]" << endl;
 		if(mheight > puckheight0) puckheight0 = mheight;
 	}
@@ -53,8 +63,11 @@ bool HoleDetector::detectHole(HAL *hal, bool runningSlow) {
 	}
 
 	// wait for puck hole end
-	while ((mheight = hal->get_height_measure()) > min_puck_height && elapsed_time < max_time)
+	while ((mheight = hal->get_height_measure()) > min_puck_height && elapsed_time > 0)
 	{
+		timer.getTime(&result);
+		elapsed_time = result.tv_sec * 1000;
+		elapsed_time += result.tv_nsec / 1000;
 		cout << "w3 (" << mheight << " > " << min_puck_height << ") [t: " << elapsed_time << "]" << endl;
 	}
 	cout << "p3" << endl;
