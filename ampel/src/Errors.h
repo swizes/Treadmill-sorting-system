dc594788b84270c708ad98b61efa7b9c9828a6aa
Roland Meo <roland.meo@haw-hamburg.de>
2015-06-24 17:22:16 +0200

    Errors.* updated - added error_print()

--- ampel/src/Errors.h
+++ ampel/src/Errors.h
@@ -7,26 +7,26 @@
  
 
  // TimeOuts
- #define ERROR_TIMOUT_Birth2RtoH    100 /* timeout from "Birth" to "Road to Height" */
- #define ERROR_TIMOUT_RtoH2HM       101 /* timeout from "Road to Height" to "Height Measurement" */
- #define ERROR_TIMOUT_HM2SfH        102 /* timeout from "Height Measurement" to "Search for Hole" */
- #define ERROR_TIMOUT_SfH2RtoM      103 /* timeout from "Search for Hole" to "Road to Metal" */
- #define ERROR_TIMOUT_RtoM2IiG      104 /* timeout from "Road to Metal" to "Is in Gate" */
- #define ERROR_TIMOUT_IiG2RtoE      105 /* timeout from "Is in Gate" to "Road to Exit" */
- #define ERROR_TIMOUT_SfH2RtoSO     106 /* timeout from "Search for Hole" to "Road to Sorting Out" */
- #define ERROR_TIMOUT_RtoSO2FP      107 /* timeout from "Road to Sorting Out" to "Flipping Puck" */
- #define ERROR_TIMOUT_FP2IiS        108 /* timeout from "Flipping Puck" to "Is In Slide" */
+ #define ERROR_TIMOUT_Birth2RtoH    100 /* timeout from <Birth> to <Road to Height> */
+ #define ERROR_TIMOUT_RtoH2HM       101 /* timeout from <Road to Height> to <Height Measurement> */
+ #define ERROR_TIMOUT_HM2SfH        102 /* timeout from <Height Measurement> to <Search for Hole> */
+ #define ERROR_TIMOUT_SfH2RtoM      103 /* timeout from <Search for Hole> to <Road to Metal> */
+ #define ERROR_TIMOUT_RtoM2IiG      104 /* timeout from <Road to Metal> to <Is in Gate> */
+ #define ERROR_TIMOUT_IiG2RtoE      105 /* timeout from <Is in Gate> to <Road to Exit> */
+ #define ERROR_TIMOUT_SfH2RtoSO     106 /* timeout from <Search for Hole> to <Road to Sorting Out> */
+ #define ERROR_TIMOUT_RtoSO2FP      107 /* timeout from <Road to Sorting Out> to <Flipping Puck> */
+ #define ERROR_TIMOUT_FP2IiS        108 /* timeout from <Flipping Puck> to <Is In Slide> */
  
  // TooSoon
- #define ERROR_2SOON_Birth2RtoH    200 /* too soon from "Birth" to "Road to Height" */
- #define ERROR_2SOON_RtoH2HM       201 /* too soon from "Road to Height" to "Height Measurement" */
- #define ERROR_2SOON_HM2SfH        202 /* too soon from "Height Measurement" to "Search for Hole" */
- #define ERROR_2SOON_SfH2RtoM      203 /* too soon from "Search for Hole" to "Road to Metal" */
- #define ERROR_2SOON_RtoM2IiG      204 /* too soon from "Road to Metal" to "Is in Gate" */
- #define ERROR_2SOON_IiG2RtoE      205 /* too soon from "Is in Gate" to "Road to Exit" */
- #define ERROR_2SOON_SfH2RtoSO     206 /* too soon from "Search for Hole" to "Road to Sorting Out" */
- #define ERROR_2SOON_RtoSO2FP      207 /* too soon from "Road to Sorting Out" to "Flipping Puck" */
- #define ERROR_2SOON_FP2IiS        208 /* too soon from "Flipping Puck" to "Is In Slide" */
+ #define ERROR_2SOON_Birth2RtoH    200 /* too soon from <Birth> to <Road to Height> */
+ #define ERROR_2SOON_RtoH2HM       201 /* too soon from <Road to Height> to <Height Measurement> */
+ #define ERROR_2SOON_HM2SfH        202 /* too soon from <Height Measurement> to <Search for Hole> */
+ #define ERROR_2SOON_SfH2RtoM      203 /* too soon from <Search for Hole> to <Road to Metal> */
+ #define ERROR_2SOON_RtoM2IiG      204 /* too soon from <Road to Metal> to <Is in Gate> */
+ #define ERROR_2SOON_IiG2RtoE      205 /* too soon from <Is in Gate> to <Road to Exit> */
+ #define ERROR_2SOON_SfH2RtoSO     206 /* too soon from <Search for Hole> to <Road to Sorting Out> */
+ #define ERROR_2SOON_RtoSO2FP      207 /* too soon from <Road to Sorting Out> to <Flipping Puck> */
+ #define ERROR_2SOON_FP2IiS        208 /* too soon from <Flipping Puck> to <Is In Slide> */
  
  //
  #define ERROR_SLIDEFULL            300 /* slide is full */
