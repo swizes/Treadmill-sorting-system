6da765c98b1ed584d3c05f40c5e7068e936073ba
Lukas Wendt <lukas.wendt@haw-hamburg.de>
2015-07-01 08:24:15 +0000

    Give_New_Puck.cpp edited online with Bitbucket

--- ampel/src/states/superStates/Give_New_Puck.cpp
+++ ampel/src/states/superStates/Give_New_Puck.cpp
@@ -6,46 +6,44 @@
  */
 
 #include "PuckStates.h"
+#include "ReadySend.h"
 
 
 static int rdyS = 0;
 
 Give_New_Puck::Give_New_Puck(Context* con): State::State(con){
 
+	ReadySend* rdySend = ReadySend::getInstance();
 
-	/*if(rdyS == 0){
-		rdySend.start(NULL);
+	if(rdyS == 0){
+		rdySend->start(NULL);
 		rdyS = 1;
-	}*/
-
+	}
 
 	printf("Give_New_Puck()\n");
     HAL *hal = HAL::getInstance();
 
-    //rdySend.setType(1);
-    //rdySend.stop();
-    //rdySend.start(NULL);
+    rdySend->setBusy(1);
+    rdySend->stop();
+    rdySend->start(NULL);
 
     //Receive FiFo Entry (Puck ID) from Band1
-    SerialCommunicationThread *sct = SerialCommunicationThread::getInstance();
-    Puck* puck = new Puck();
-   // puckStruct puck;
-    sct->receivePuck(puck);
-    //ser.recvPacket(&puck);
+    puckStruct puck;
+    ser.recvPacket(&puck);
 
-    cout << "Puck ID: " << puck->getId();
-    cout << "    Puck Metal: " << puck->isMetal();
-    cout << "    Puck Hole: " << puck->isHoleOnTop() << endl;
+    cout << "Puck ID: " << puck.id;
+    cout << "    Puck Metal: " << puck.metal;
+    cout << "    Puck Hole: " << puck.holeOnTop << endl;
 
 
-   // Puck* p = new Puck();
-   // p->setPuckFromStruct(puck);
-    this->con_ = new Context(puck);
+    Puck* p = new Puck();
+    p->setPuckFromStruct(puck);
+    this->con_ = new Context(p);
     this->con_->setState(this);
 
-    //rdySend.setType(0);
-    //rdySend.stop();
-    //rdySend.start(NULL);
+    rdySend->setBusy(0);
+    rdySend->stop();
+    rdySend->start(NULL);
 
     int loop = 1;
 	while(loop){
@@ -56,12 +54,12 @@ Give_New_Puck::Give_New_Puck(Context* con): State::State(con){
             hal->turn_greenLight_off();
             loop = 0;
         }
-        if( hal->is_resetButton_pushed()==1){
-            hal->band_stop();
-            // Move to State: Working_Band2
-            new (this) Ready(this->con_);
-            loop=0;
-        }
+//        if( hal->is_resetButton_pushed()==1){
+//            hal->band_stop();
+//            // Move to State: Working_Band2
+//            new (this) Ready(this->con_);
+//            loop=0;
+//        }
     }
 }
 
